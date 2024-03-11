e708f608ba5cd64bd39ef4694472d7f2d88b3ff1
bnolanhussle <155196261+bnolanhussle@users.noreply.github.com>
2024-03-11 21:32:41 +0000

    Mar 11, 2024, 9:32 PM

--- script.js
+++ script.js
@@ -16,7 +16,10 @@ console.log("Divide Result: ", divideResult);
 console.log("Second Divide Result: ", divide(35, 7));
 
 // Create a function that has no parameters and adds 2 to 3 and then returns the result.
-function add(){
-    let result = 2 + 3;
-    
+function add() {
+  let result = 2 + 3;
+  return result;
 }
+
+let addResult = add();
+console.log("Add Result: ", addResult);
